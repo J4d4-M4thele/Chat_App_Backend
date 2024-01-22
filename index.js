8d045498b9dd97acdeb6a23672452acf87336c51
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-22 16:24:24 +0200

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
-import sockets from "./socket/sockets"; 
+import sockets from "./socket/sockets.js"; 
 
 const app = express();
 const port = 4000;
--- socket/sockets.js
+++ socket/sockets.js
@@ -1,32 +1,27 @@
+import TypingController from "../controllers/TypingController.js";
+
 const sockets = (socket) => {
-    socket.on("send-message", ({message, roomId}) => {
+    const typingController = new TypingController(socket);
+
+    socket.on("send-message", ({ message, roomId }) => {
         //sending message to all connected client windows
         let skt = socket.broadcast;
         skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("message-from-server", {message});
+        skt.emit("message-from-server", { message });
     });
 
     //checks to see if user is typing their message
-    socket.on("typing-started", ({roomId}) => {
-        let skt = socket.broadcast;
-        skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("typing-started-from-server");
-    });
+    socket.on("typing-started", typingController.typingStarted);
+    socket.on("typing-stopped", typingController.typingStopped);
 
-    socket.on("typing-stopped", ({roomId}) => {
-        let skt = socket.broadcast;
-        skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("typing-stopped-from-server");
-    });
-
-    socket.on("join-room", ({roomId}) => {
+    socket.on("join-room", ({ roomId }) => {
         console.log('Joining room');
         socket.join(roomId);
     });
 
     socket.on("disconnect", (socket) => {
         console.log("User left");
-   });
-} 
+    });
+}
 
 export default sockets; 
