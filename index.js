a2fd752a4f9e181a4ab3c90f4bf4c231301db3d2
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-22 16:07:19 +0200

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import http from "http";
 import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
+import sockets from "./socket/sockets"; 
+
 const app = express();
 const port = 4000;
 
@@ -22,36 +24,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-    socket.on("send-message", ({message, roomId}) => {
-        //sending message to all connected client windows
-        let skt = socket.broadcast;
-        skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("message-from-server", {message});
-    });
-
-    //checks to see if user is typing their message
-    socket.on("typing-started", ({roomId}) => {
-        let skt = socket.broadcast;
-        skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("typing-started-from-server");
-    });
-
-    socket.on("typing-stopped", ({roomId}) => {
-        let skt = socket.broadcast;
-        skt = roomId ? skt.to(roomId) : skt;
-        skt.emit("typing-stopped-from-server");
-    });
-
-    socket.on("join-room", ({roomId}) => {
-        console.log('Joining room');
-        socket.join(roomId);
-    });
-
-    socket.on("disconnect", (socket) => {
-        console.log("User left");
-   });
-});
+io.on("connection", sockets);
 
 httpServer.listen(port, () => {
     console.log(`Server is running at http://localhost:4000`)
