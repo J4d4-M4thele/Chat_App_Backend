b31d00e356690cd70c656d1229087c377aaddbbd
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-23 11:30:34 +0200

    first commit

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -1,7 +1,11 @@
 import BaseController from "./BaseController.js";
 
-export default class RoomController extends BaseController{
+export default class RoomController extends BaseController {
     joinRoom = ({ roomId }) => {
         this.socket.join(roomId);
+    }
+
+    newRoomCreated = ({ roomId }) => {
+        this.socket.broadcast.emit('new-room-created', { roomId });
     }
 }
